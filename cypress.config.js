08b1c0338859aaef1df42637001afc401b9b0654
Yevhen Voitsytskyi <yevhen.voitsytskyi@nixs.com>
2025-10-30 21:13:37 +0100

    config changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,11 @@
 const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
-const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // подключаем cucumber preprocessor
       on("file:preprocessor", cucumber());
-
-      // подключаем allure writer
-      allureWriter(on, config);
-
-      return config;
     },
     specPattern: "cypress/specs/*.feature"
-  },
-  env: {
-    allure: true,
-    allureResultsPath: "allure-results",
-  },
+  }
 });
