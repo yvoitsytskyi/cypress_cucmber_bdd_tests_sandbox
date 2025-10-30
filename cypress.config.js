cdfe42d941e68d801e515974f3e6cfdfbce2e29f
Yevhen Voitsytskyi <yevhen.voitsytskyi@nixs.com>
2025-10-30 16:07:06 +0100

    added necessary files
    added necessary files 2
    
    allure report added
    
    updated workflows file
    
    renamed branch
    
    changes added to workflows file
    
    changed main branch
    
    changes
    
    changes
    
    new changes
    
    deleted files
    
    changes
    
    changes
    
    last changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,22 @@
-const cucumber = require('cypress-cucumber-preprocessor').default
 const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('file:preprocessor', cucumber())
+      // подключаем cucumber preprocessor
+      on("file:preprocessor", cucumber());
+
+      // подключаем allure writer
+      allureWriter(on, config);
+
+      return config;
     },
-    specPattern: "cypress/specs/*.feature",
+    specPattern: "cypress/specs/*.feature"
+  },
+  env: {
+    allure: true,
+    allureResultsPath: "allure-results",
   },
 });
